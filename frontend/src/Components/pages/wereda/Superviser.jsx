07028117962d167ecba933b3e8c2655d68f97a02
Etbur Software Development Center <wedajebimrew7@gmail.com>
2025-10-03 00:30:02 -0600

    Update Superviser.jsx

--- frontend/src/Components/pages/wereda/Superviser.jsx
+++ frontend/src/Components/pages/wereda/Superviser.jsx
@@ -23,7 +23,7 @@ const SupervisorSchool = () => {
   });
 
   const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://eschooladmin.etbur.com/api/",
     headers: {
       Authorization: `Bearer ${token}`,
       "Content-Type": "application/json",
